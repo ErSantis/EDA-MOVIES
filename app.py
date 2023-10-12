a69755aa659630e2bba188e21ff3f55335f907cf
Jandres02 <avjohan@uninorte.edu.co>
2023-10-12 15:00:14 -0500

    changes in 1st graphic

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from tvshows import donut, sunburst, bar, plot_map
 # Crear la aplicación Dash
 app = dash.Dash(__name__)
 
-# Estilos de CSS con una paleta de colores oscuros diferentes
+# Estilos de CSS con una paleta de colores diferentes
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Aplicar los estilos CSS
@@ -20,9 +20,27 @@ app.layout = html.Div([
         html.H1("EDA TV SHOWS AND SERIES", style={'textAlign': 'center', 'color': 'white'}),
     ], style={'backgroundColor': '#1C2833', 'padding': '20px'}),
 
-    # Fila 1: Gráfico de género de los 10 mejores programas de televisión
+    # Fila 1: Gráfico de género de los programas de televisión principales
     html.Div([
-        html.H2("Genre of the top 10 TV Shows", style={'color': 'white'}),
+        html.H2("Genre of the top TV Shows", style={'color': 'white'}),
+        html.Label("Seleccione el top que desea ver", style={'color': 'white', 'padding': '5px'}),
+        dcc.RadioItems(
+            id='top-selection',
+            options=[
+                {'label': 'Mejor', 'value': 'best'},
+                {'label': 'Peor', 'value': 'worst'}
+            ],
+            value='best',
+            labelStyle={'display': 'inline-block', 'padding': '5px'}
+        ),
+        dcc.RangeSlider(
+            id='top-n-slider',
+            min=1,
+            max=10,
+            step=1,
+            value=[1, 10],
+            marks={i: str(i) for i in range(1, 11)},
+        ),
         dcc.Dropdown(
             id='dropdown',
             options=[{'label': platform, 'value': platform} for platform in ['Netflix', 'Prime Video', 'HBO', 'Disney+', 'BBC One', 'YouTube']],
@@ -34,7 +52,7 @@ app.layout = html.Div([
             style={'width': '100%'}
         )
     ], style={'backgroundColor': '#39424E', 'padding': '20px', 'border-radius': '5px'}),
-
+    
     # Fila 2: Gráfico de distribución de programas en plataformas
     html.Div([
         html.H2("Distribution of TV Shows on Platforms", style={'color': 'white'}),
@@ -79,10 +97,12 @@ app.layout = html.Div([
 # Definir funciones de devolución de llamada
 @app.callback(
     Output('bar-graph', 'figure'),
-    Input('dropdown', 'value')
+    Input('dropdown', 'value'),
+    Input('top-n-slider', 'value'),
+    Input('top-selection', 'value')
 )
-def actualizar_grafico(value):
-    return sunburst(value)
+def actualizar_grafico(selected_platform, top_n, top_selection):
+    return sunburst(selected_platform, top_n, top_selection)
 
 @app.callback(
     Output('graph2', 'figure'),

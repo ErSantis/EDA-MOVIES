f8977e67fbeae70f6696c8f1a026460968830b33
Elkin <ersantis@uninorte.edu.co>
2023-10-16 13:47:12 -0500

    ready to demo
    Co-authored-by: Jaymed Daniel Linero Caro <jaymedl@uninorte.edu.co>

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from tvshows import donut, sunburst, bar, plot_map, get_df, histo
 external_stylesheets = [
     "https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
 ]
-external_stylesheets2 = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
+df = get_df()
 app = dash.Dash(__name__)
 
 # Aplicar los estilos CSS
@@ -28,44 +27,48 @@ div_style = {
 }
 
 app.layout = html.Div([
-   
-   #Titulo
+
+    #Titulo
     html.Div([
-        html.H1("EDA TV SHOWS", className='custom-font-lilita', style={'textAlign': 'center', 'color': 'white'}),
+        html.H1("Exploratory Data Analysis on TV Shows", className='custom-font-lilita', style={'textAlign': 'center', 'color': 'white'}),
     ], style={'backgroundColor': '#39B6A9', 'padding': '20px', 'text-align': 'center'}),
 
-    # Div para la gráfica de género de TV Shows
-    html.Div([
-        
-        #Div de titulo y opciones
-        html.Div([
-            html.H2("Genre of the top TV Shows", className='custom-font-lilita', style={'color': 'white'})
-        ], style={**div_style, 'backgroundColor': '#007266', 'border-radius': '5px', 'text-align': 'center'}),
-        
-        html.Div([ 
-            html.Label("Seleccione el top que desea ver", className='custom-font-lilita', style={'color': 'white', 'padding': '10px'}),
-            dcc.Input(
-                id='top-n-dropdown',
-                type='number',
-                min=1,
-                max=10,
-                value=10,
-                style={'width': '69px'}
-            ),
-        ], style={'display': 'flex', 'align-items': 'center'}),
-        
-        dcc.Dropdown(
-                id='dropdown',
-                options=[{'label': platform, 'value': platform} for platform in ['Netflix', 'Prime Video', 'HBO', 'Disney+', 'BBC One', 'YouTube']],
-                value='Netflix',
-                style={'width': '100%'}
-        ),
-        dcc.Graph(
-            id='bar-graph',
-            style={'width': '100%','height': '100%'}
-        )
-    ], style={**div_style, 'border-radius': '5px','backgroundColor': '#007266', 'text-align': 'center'}),
 
+    html.Div([
+    
+        html.Div([
+        
+            html.H2("Genre of the top TV Shows", className='custom-font-lilita', style={'color': 'white', 'text-align': 'center'}),
+
+            # Div para el campo "Top" y dropdown
+            html.Div([
+                html.Label("Top", className='custom-font-lilita', style={'color': 'white', 'padding': '10px', 'text-align': 'right'}),
+                dcc.Input(
+                    id='top-n-dropdown',
+                    type='number',
+                    min=1,
+                    max=10,
+                    value=10,
+                    style={'width': '30px','font-family': 'Nunito'}
+                )
+            ], style={'position':'absolute','z-index':'2','top':'0','right':'5px'}),
+        ], style={ 'justify-content': 'space-between', 'text-align': 'center', 'align-items': 'center','position':'relative'}),
+        
+
+        dcc.Dropdown(
+            id='dropdown',
+            options=[{'label': platform, 'value': platform} for platform in ['Netflix', 'Prime Video', 'HBO', 'Disney+', 'BBC One', 'YouTube']],
+            value='Netflix',
+            style={'width': '100%','font-family': 'Nunito','font-size':'18'}
+        ),
+
+        # Gráfico
+        dcc.Graph(
+            id='sunburst-graph',
+            style={'width': '100%', 'height': '100%'}
+        )
+        ], style={**div_style, 'border-radius': '5px', 'backgroundColor': '#007266', 'text-align': 'center'}),
+    
     # Div para la gráfica de distribución de TV Shows en plataformas
     html.Div([
         html.H2("Distribution of TV Shows on Platforms", className='custom-font-lilita', style={'color': 'white'}),
@@ -83,10 +86,11 @@ app.layout = html.Div([
             id='dropdown2',
             options=[{'label': platform, 'value': platform} for platform in ['Netflix', 'Prime Video', 'HBO', 'Disney+', 'BBC One', 'YouTube']],
             value='Netflix',
-            style={'width': '100%'}
+            style={'width': '100%','font-family': 'Nunito','font-size':'18'}
         ),
         dcc.Graph(
-            id='graph2',
+            id='bar-graph',
+            figure = bar('Netflix'),
             style={'width': '100%'}
         )
     ], style={**div_style, 'backgroundColor': '#007266', 'border-radius': '5px', 'text-align': 'center'}),
@@ -98,41 +102,46 @@ app.layout = html.Div([
             id='dropdown3',
             options=[{'label': platform, 'value': platform} for platform in ['Netflix', 'Prime Video', 'HBO', 'Disney+', 'BBC One', 'YouTube']],
             value='Netflix',
-            style={'width': '100%'}
+            style={'width': '100%','font-family': 'Nunito','font-size':'18'}
         ),
         dcc.Graph(
             id='map-graph',
+            figure = plot_map('Netflix'),
             style={'width': '100%'}
         )
     ], style={**div_style, 'backgroundColor': '#007266', 'border-radius': '5px', 'text-align': 'center'}),
 
-    #Tabla
+  #Tabla
     html.Div([
-    html.H2("Series Status", className='custom-font-lilita', style={'color': 'white'}),
-    dcc.RadioItems(
-        options=[
-            {'label': 'Planned', 'value': 'Planned'},
-            {'label': 'In Production', 'value': 'In Production'},
-            {'label': 'Returning Series', 'value': 'Returning Series'},
-            {'label': 'Canceled', 'value': 'Canceled'},
-            {'label': 'Ended', 'value': 'Ended'},
-            {'label': 'Pilot', 'value': 'Pilot'}
-        ],
-        value='Ended',
-        inline=True,
-        id='my-radio-buttons-final',
-        style={'color': 'white'},
-        labelStyle={'font-size': '20px'}
-    )
-], style={'backgroundColor': '#007266', 'border-radius': '5px', 'text-align': 'center', 'width': '100%'}),
+        html.H2("Series Status", className='custom-font-lilita', style={'color': 'white'}),
+        
+    ], style={'backgroundColor': '#007266', 'border-radius': '5px', 'text-align': 'center', 'width': '100%'}),
 
     html.Div(className='row', children=[
         html.Div(className='six columns', children=[
             dash_table.DataTable(data=get_df().to_dict('records'), page_size=11, style_table={'overflowX': 'auto'},
-                                style_cell={'font-family': 'Your-Font-Family', 'font-size': '24px','text-align':'center'})
+                                style_cell={'font-family': 'Nunito', 'font-size': '24px','text-align':'center'})
         ]),
+        html.Div([
+            dcc.RadioItems(
+            options=[
+                {'label': 'Planned', 'value': 'Planned'},
+                {'label': 'In Production', 'value': 'In Production'},
+                {'label': 'Returning Series', 'value': 'Returning Series'},
+                {'label': 'Canceled', 'value': 'Canceled'},
+                {'label': 'Ended', 'value': 'Ended'},
+                {'label': 'Pilot', 'value': 'Pilot'}
+            ],
+            value='Ended',
+            inline=True,
+            id='radio-buttons',
+            style={'color': 'white' },
+            labelStyle={'font-size': '20px'},
+            className='custom-font-lilita'
+            )
+        ], style={'backgroundColor': '#007266', 'border-radius': '5px', 'text-align': 'center', 'width': '100%', 'padding-bottom': '5px'}),
         html.Div(className='six columns', children=[
-            dcc.Graph(figure={}, id='histo-chart-final')
+            dcc.Graph(figure=histo(df,'Ended'), id='histo')
         ])
     ]),
     
@@ -149,14 +158,15 @@ app.layout = html.Div([
 
         # Columna central
         html.Div([
-            html.P("© 2023 Linero, Santis y Vergara. Todos los derechos reservados.", className='custom-input-quicksand'),  # Copyright
-            html.P("Base de datos recuperada de Kaggle: ", className='custom-input-quicksand', style={'display': 'inline'}),
-            dcc.Link("Clic Aquí", className='custom-input-quicksand', href='https://www.kaggle.com/datasets/asaniczka/full-tmdb-tv-shows-dataset-2023-150k-shows', style={'text-decoration': 'underline', 'color': 'blue'})
+            html.P("© 2023 Linero, Santis y Vergara. Todos los derechos reservados.", className='custom-input-quicksand', style={'border-color': '#39B6A9'}),  # Copyright
+            html.P("Base de datos recuperada de Kaggle: ", className='custom-input-quicksand', style={'display': 'inline', 'border-color': '#39B6A9'}),
+            dcc.Link("Clic Aquí", className='custom-input-quicksand', href='https://www.kaggle.com/datasets/asaniczka/full-tmdb-tv-shows-dataset-2023-150k-shows',
+                    style={'text-decoration': 'underline', 'color': 'blue', 'border-color': '#39B6A9'})
         ], style={'flex': 1, 'text-align': 'center', 'margin-left': '15%'}),
 
         # Columna derecha
         html.Div([
-            html.P("Enlace al repositorio", className='custom-input-quicksand'),
+            html.P("Enlace al repositorio", className='custom-input-quicksand', style={'border-color': '#39B6A9'}),
             html.A(
                 html.Img(src='/assets/GitHub-Emblem.png', alt='Enlace al repositorio de GitHub', style={'width': 'auto', 'height': '50px', 'margin-right': '10px'}),
                 href='https://github.com/ErSantis/EDA-MOVIES' #Para acceder al repositorio
@@ -167,19 +177,20 @@ app.layout = html.Div([
 
 
 @app.callback(
-    Output('bar-graph', 'figure'),
+    Output('sunburst-graph', 'figure'),
     Input('top-n-dropdown', 'value'),
     Input('dropdown', 'value')
 )
 def actualizar_grafico(valor_n_dropdown, valor_dropdown):
     return sunburst(valor_n_dropdown, valor_dropdown)
 
+initial = 'Netflix'
 @app.callback(
-    Output('graph2', 'figure'),
+    Output('bar-graph', 'figure'),
     Input('dropdown2', 'value')
 )
-def actualizar_grafico2(value):
-    return bar(value)
+def actualizar_grafico2(valor_dropdown):
+    return bar(valor_dropdown)
 
 @app.callback(
     Output('map-graph', 'figure'),
@@ -189,12 +200,12 @@ def actualizar_grafico3(value):
     return plot_map(value)
 
 @app.callback(
-    Output(component_id='histo-chart-final', component_property='figure'),
-    Input(component_id='my-radio-buttons-final', component_property='value')
+    Output('histo', 'figure'),
+    Input('radio-buttons', 'value')
 )
+
 def update_graph(col_chosen):
-    print(col_chosen)
-    return histo(get_df(),col_chosen)
+    return histo(df,col_chosen)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- tvshows.py
+++ tvshows.py
@@ -29,6 +29,15 @@ except Exception as ex:
 
 
 def values_count(value):
+    """
+    The function `values_count` retrieves the count of movies from a database table where the networks
+    column contains a specific value.
+    
+    :param value: The `value` parameter is a string that represents a value to search for in the
+    `networks` column of a table called `movies`
+    :return: the count of rows in the "movies" table where the "networks" column contains the specified
+    value.
+    """
     try:
         cursor = connection.cursor()
         cursor.execute(("SELECT Count(*) FROM movies as D WHERE D.networks like" + f'\'%{value}%\''))
@@ -42,6 +51,11 @@ def values_count(value):
 
 
 def donut():
+    """
+    The `donut` function creates a donut chart using the `plotly` library to visualize the sizes of
+    different streaming services.
+    :return: a pie chart figure object.
+    """
     labels = ['Netflix', 'Prime video', 'HBO', 'Disney+','BBC One', 'YouTube','NBC','FOX','Discovery','MTV']
     colors = ["#ff6b6b", "#EDE603", "#6C15B9", "#72efdd","#003880", "#D8047E","#DC3A0E","#0E1EDC","#0EDC30","#0E94DC"]
     sizes = []
@@ -56,7 +70,7 @@ def donut():
     )
     fig.update_layout(
         paper_bgcolor='#ffffff',
-        height=862
+        height=896
         #height=740  # Cambia el fondo a azul
     )
     fig.update_traces(textinfo='percent+label')
@@ -68,8 +82,19 @@ def donut():
 # In[8]:
 
 
-def vote_count(value):
+def vote_count(value): 
+    """
+    The function `vote_count` retrieves the name, genres, and vote count of movies from a database table
+    called "movies" that match a given network value and have a non-zero vote count, ordered by vote
+    count in descending order.
     
+    :param value: The `value` parameter is a string that represents the network name. The function
+    retrieves the name, genres, and vote count of movies from a database table called "movies" where the
+    network name matches the provided value. The movies are sorted in descending order based on their
+    vote count
+    :return: the rows from the database that match the given value and have a non-zero vote count,
+    sorted in descending order by vote count.
+    """
     try:
         cursor = connection.cursor()
         cursor.execute("SELECT D.name ,D.genres, D.vote_count FROM movies as D WHERE D.networks like" + f'\'%{value}%\' and D.vote_count !=0 order by D.vote_count DESC')
@@ -84,6 +109,17 @@ def vote_count(value):
 
 
 def sunburst(val1, net):
+    """
+    The `sunburst` function takes in a value and a network, and returns a sunburst plot based on the
+    data in the network.
+    
+    :param val1: The parameter `val1` is the number of rows you want to include in the sunburst chart.
+    It determines how many data points will be displayed in the chart
+    :param net: The `net` parameter is a list of tuples containing information about different streaming
+    platforms. Each tuple contains the name of the platform, its genre, and the number of votes it has
+    received
+    :return: a Plotly sunburst chart object.
+    """
     c = net
     df = pd.DataFrame()
     net = vote_count(net)
@@ -103,8 +139,10 @@ def sunburst(val1, net):
     )
     fig.update_layout(
         paper_bgcolor='#ffffff',
-        height=770,
-        title_font=dict(family='Arial', size=18)
+        height=862,
+        title_font=dict(family='Arial', size=18),
+        font_family='Times New Roman',
+        font_size = 18
     )
      # Configura el rango inicial de zoom
     fig.update_xaxes(range=[0, 1])  # Ajusta el rango del eje X
@@ -202,7 +240,8 @@ def bar(value):
         xaxis_title='Language',
         yaxis_title='Number of TV shows',
         showlegend=False,
-        plot_bgcolor='white'
+        plot_bgcolor='white',
+        height=650
     )
     c = {'Netflix':'#ff6b6b',
          'Prime Video':'#00A8E1',
@@ -223,15 +262,6 @@ def map(value):
     except Exception as ex:
         print(ex)
 
-def create_df_map(value):
-    df = pd.DataFrame()
-    net = map(value)
-    df['name'] = [item[0] for item in net]
-    df['country'] = [item[1] for item in net]
-    df['votes'] = [item[2] for item in net]
-    df_expand = df.assign(country=df['country'].str.split(',')).explode('country')
-    df_country_rating = df_expand.groupby('country')['votes'].mean().reset_index()
-    return df_country_rating
 
 def create_df_map(value):
     df = pd.DataFrame()
@@ -257,7 +287,7 @@ def plot_map(value):
     df_country_rating = create_df_map(value)
     df_country_rating['country'] = df_country_rating['country'].apply(get_country_name)
     fig = px.scatter_geo(df_country_rating, locations='country', locationmode='country names', color='votes',
-                        title=f'Average Rating by Country for {value} Shows', scope='world',size='votes')
+                        title=f'Average Rating by Country for {value} Shows', scope='world',size='votes', height=650)
 
     return fig    
 
